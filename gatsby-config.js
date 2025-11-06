aa96a8cce80d2431acc62f3becdf2f8ac14659d1
ejs95 <eojaeseong950@gmail.com>
2025-11-06 22:18:49 +0900

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,11 @@
+process.env.GATSBY_CPU_COUNT = "4";
+process.env.GATSBY_LOGGER = "yurnalist";
+process.env.GATSBY_EXPERIMENTAL_PAGE_BUILD_ON_DATA_CHANGES = "true";
+process.env.GATSBY_EXPERIMENTAL_DISABLE_CACHE = "false";
+process.env.GATSBY_CLI_FORCE_COLOR = "true";
+process.env.GATSBY_CACHE_DIR = "C:\\gatsby_cache"; 
 module.exports = {
+  pathPrefix: "/WEDDING_INVITATION",
   siteMetadata: {
     title: `weddingInvitation`,
     siteUrl: `https://www.yourdomain.tld`
